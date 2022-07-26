55bd91f45efdb7b7c1d2206a9882c825b4f36661
Leonardo Jaques <jaques.projetos@outlook.com>
2022-07-26 00:45:04 -0300

    feat: add tasks and notasks values

---
+++ src/components/Todo/Task.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <v-list-item
+      @click="store.commit('taskDone', task.id)"
+      :class="{ 'blue light-5': task.done }"
+    >
+      <template v-slot:default>
+        <v-list-item-action>
+          <v-checkbox :input-value="task.done" color="primary"></v-checkbox>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{ 'text-decoration-line-through': task.done }"
+            >{{ task.title }}</v-list-item-title
+          >
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn @click.stop="store.commit('deleteTask', task.id)" icon>
+            <v-icon color="primary lighten-1">mdi-delete</v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </template>
+    </v-list-item>
+    <v-divider></v-divider>
+  </div>
+</template>
+<script>
+import store from "@/store";
+
+export default {
+  name: "Task",
+  props: ["task"],
+  data: () => {
+    return {
+      store,
+     };
+  },
+};
+</script>
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,12 +1,8 @@
 <template lang="html">
   <div class="home">
     <field-add-task />
-    <list-tasks
-    v-if="store.state.tasks.length" />
-    <div v-else class="no-tasks">
-      <v-icon size="100" color="primary ">mdi-check</v-icon>
-      <div class="text-h5 primary--text">No tasks</div>
-    </div>
+    <list-tasks v-if="store.state.tasks.length" />
+    <no-task v-else />
   </div>
 </template>
 
@@ -14,27 +10,21 @@
 import store from "@/store";
 import FieldAddTask from "@/components/Todo/FieldAddTask.vue";
 import ListTasks from "@/components/Todo/ListTasks.vue";
+import NoTasks from "@/components/Todo/NoTasks.vue";
 
 export default {
   name: "Home",
   data: () => {
     return {
       store,
-    }
+    };
   },
   components: {
     "field-add-task": FieldAddTask,
     "list-tasks": ListTasks,
+    "no-task": NoTasks,
   },
 };
 </script>
 
-<style>
-.no-tasks {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  opacity: 0.5;
-}
-</style>
+
